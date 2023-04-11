3261cf22c6e3e0a62408c24eeb4081e0fe801911
A_ito <BoxPistols@users.noreply.github.com>
2023-04-12 05:04:10 +0900

    Ft/#5 2 re refactor table with type script async (#8)
    * small update

--- src/AgGrid/AgTableBasic.tsx
+++ src/AgGrid/AgTableBasic.tsx
@@ -28,13 +28,14 @@ const AgTableBasic = () => {
   const defaultColDef = useMemo(
     () => ({
       sortable: true,
+      filter: true,
     }),
     []
   );
 
   // セルクリック時の処理
-  const handleCellClicked = useCallback((event: any) => {
-    console.log("cellClicked", event);
+  const handleCellClicked = useCallback((e: any) => {
+    console.log("cellClicked", e);
   }, []);
 
   // データの読み込み処理
@@ -45,7 +46,7 @@ const AgTableBasic = () => {
   }, []);
 
   // Clear Selectの処理
-  const handleButtonClicked = useCallback(() => {
+  const handleButtonClicked = useCallback((_e: any) => {
     gridRef.current?.api.deselectAll();
   }, []);
 
@@ -70,7 +71,6 @@ const AgTableBasic = () => {
         <Button variant="outlined" sx={{ mt: 1 }} onClick={handleButtonClicked}>
           Clear Select
         </Button>
-        {/* <button onClick={handleButtonClicked}>Clear Select</button> */}
       </main>
     </div>
   );
